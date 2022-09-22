6b3a6ca574275b0a563531f9468e5046fbef1424
Phil Fernandez <philvfernandez@gmail.com>
2022-09-22 17:25:06 -0600

    Added thymeleaf dependency to project and added simple dynamic example template page.

--- src/main/java/com/easybytes/easyschool/controllers/HomeController.java
+++ src/main/java/com/easybytes/easyschool/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package com.easybytes.easyschool.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value={"","/","/home"})
-    public String displayHomePage() {
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Phil Fernandez");
         return "home.html";
     }
 }
