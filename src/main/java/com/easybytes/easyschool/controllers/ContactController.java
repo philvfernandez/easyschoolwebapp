1670266019a65c32a0247f60a70fcbc820d36d91
Phil Fernandez <philvfernandez@gmail.com>
2022-10-17 16:48:05 -0600

    Added controller method to save contact form.

--- src/main/java/com/easybytes/easyschool/controllers/ContactController.java
+++ src/main/java/com/easybytes/easyschool/controllers/ContactController.java
@@ -1,13 +1,33 @@
 package com.easybytes.easyschool.controllers;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class ContactController {
+
+    //This can be used in place of the @Slf4j annotation.
+    //private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
     @RequestMapping("/contact")
     public String displayContactPage() {
         return "contact.html";
+    }
+
+    @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+                                    @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
+        log.info("Name: " + name);
+        log.info("Mobile Number: " + mobileNum);
+        log.info("Email Address: " + email);
+        log.info("Subject: " + subject);
+        log.info("Message: " + message);
+        return new ModelAndView("redirect:/contact");
     }
 }
