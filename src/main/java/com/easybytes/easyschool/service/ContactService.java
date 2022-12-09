83267ba595e427d2f7e23374ccefaa5172aa8f6b
Phil Fernandez <philvfernandez@gmail.com>
2022-12-08 21:42:12 -0700

    Added session scope example.

--- src/main/java/com/easybytes/easyschool/service/ContactService.java
+++ src/main/java/com/easybytes/easyschool/service/ContactService.java
@@ -4,10 +4,12 @@ import com.easybytes.easyschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Slf4j
 @Service
-@RequestScope
+//@RequestScope
+@SessionScope
 public class ContactService {
 
     /**
