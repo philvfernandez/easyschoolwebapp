156008d67ab6dd0b99cd490fbc52cfb99b640466
Phil Fernandez <philvfernandez@gmail.com>
2022-12-18 18:18:07 -0700

    Added initial configuration for H2 DB and created schema.

--- src/main/java/com/easybytes/easyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/easybytes/easyschool/config/ProjectSecurityConfig.java
@@ -42,22 +42,25 @@ public class ProjectSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().ignoringAntMatchers("/saveMsg").and()
+        http.csrf().ignoringAntMatchers("/saveMsg").ignoringAntMatchers("/h2-console/**").and()
                 .authorizeHttpRequests()
                 .mvcMatchers("/dashboard").authenticated()
                 .mvcMatchers("/home").permitAll()
-                .mvcMatchers("/holidays/*").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
                 .mvcMatchers("/contact").permitAll()
                 .mvcMatchers("/saveMsg").permitAll()
                 .mvcMatchers("/courses").permitAll()
                 .mvcMatchers("/about").permitAll()
                 .mvcMatchers("/login").permitAll()
                 .mvcMatchers("/logout").permitAll()
+                .mvcMatchers("/h2-console/**").permitAll()
                 .and().formLogin().loginPage("/login")
                 .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
                 .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
+                //.and().authorizeRequests().antMatchers("/h2-console/**").permitAll()
                 .and().httpBasic();
 
+        http.headers().frameOptions().disable();
         return http.build();
     }
 
