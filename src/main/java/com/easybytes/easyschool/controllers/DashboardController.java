e8364125a7b677757b02ede2ba34b91e2ebfadf9
Phil Fernandez <philvfernandez@gmail.com>
2023-01-10 14:06:40 -0700

    Added code to populate the profile page from the DB.

--- src/main/java/com/easybytes/easyschool/controllers/DashboardController.java
+++ src/main/java/com/easybytes/easyschool/controllers/DashboardController.java
@@ -1,18 +1,32 @@
 package com.easybytes.easyschool.controllers;
 
+import com.easybytes.easyschool.model.Person;
+import com.easybytes.easyschool.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @Controller
 public class DashboardController {
 
+    @Autowired
+    PersonRepository personRepository;
+
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model, Authentication authentication) {
-        model.addAttribute("username", authentication.getName());
+    public String displayDashboard(Model model, Authentication authentication, HttpSession httpSession) {
+        Person person = personRepository.readByEmail(authentication.getName());
+
+        //set person object in session.
+        httpSession.setAttribute("loggedInPerson", person);
+
+
+        model.addAttribute("username", person.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
 
         return "dashboard.html";
--- src/main/java/com/easybytes/easyschool/controllers/ProfileController.java
+++ src/main/java/com/easybytes/easyschool/controllers/ProfileController.java
@@ -1,5 +1,6 @@
 package com.easybytes.easyschool.controllers;
 
+import com.easybytes.easyschool.model.Person;
 import com.easybytes.easyschool.model.Profile;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -7,13 +8,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @Controller
 public class ProfileController {
 
     @RequestMapping("/displayProfile")
-    public ModelAndView displayMessages(Model model) {
+    public ModelAndView displayMessages(Model model, HttpSession httpSession) {
+
+        //Get Person object from session
+        Person person = (Person) httpSession.getAttribute("loggedInPerson");
+
         Profile profile = new Profile();
+        profile.setName(person.getName());
+        profile.setMobileNumber(person.getMobileNumber());
+        profile.setEmail(person.getEmail());
+
+        if(person.getAddress() != null && person.getAddress().getAddressId() > 0) {
+            profile.setAddress1(person.getAddress().getAddress1());
+            profile.setAddress2(person.getAddress().getAddress2());
+            profile.setCity(person.getAddress().getCity());
+            profile.setState(person.getAddress().getState());
+            profile.setZipCode(person.getAddress().getZipCode());
+        }
+
         ModelAndView modelAndView = new ModelAndView("profile.html");
         modelAndView.addObject("profile",profile);
         return modelAndView;
