a4fc9dd36f7604ac3ea14bbef8a28386fe25275f
Phil Fernandez <philvfernandez@gmail.com>
2023-01-11 11:50:07 -0700

    Implemented OneToMany and ManyToOne configurations inside entity classes.

---
+++ src/main/java/com/easybytes/easyschool/model/EazyClass.java
@@ -0,0 +1,28 @@
+package com.easybytes.easyschool.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "class")
+public class EazyClass extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int classId;
+
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must be at least 3 characters long")
+    private String name;
+
+    @OneToMany(mappedBy = "eazyClass", fetch = FetchType.LAZY,
+               cascade = CascadeType.PERSIST, targetEntity = Person.class)
+    private Set<Person> persons;
+}
