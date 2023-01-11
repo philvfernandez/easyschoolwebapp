1d818823a3f5eb3bc4a7cb6d681469680399ad47
Phil Fernandez <philvfernandez@gmail.com>
2023-01-11 14:22:21 -0700

    Implemented add and delete classes and functionality.

--- src/main/java/com/easybytes/easyschool/controllers/AdminController.java
+++ src/main/java/com/easybytes/easyschool/controllers/AdminController.java
@@ -1,21 +1,65 @@
 package com.easybytes.easyschool.controllers;
 
+import com.easybytes.easyschool.EasySchoolWebApplication;
 import com.easybytes.easyschool.model.EazyClass;
+import com.easybytes.easyschool.model.Person;
+import com.easybytes.easyschool.repository.EazyClassRepository;
+import com.easybytes.easyschool.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
 @RequestMapping("admin")
 public class AdminController {
 
+    @Autowired
+    EazyClassRepository eazyClassRepository;
+
+    @Autowired
+    PersonRepository personRepository;
+
     @RequestMapping("/displayClasses")
     public ModelAndView displayClasses(Model model) {
+        List<EazyClass> eazyClasses = eazyClassRepository.findAll();
         ModelAndView modelAndView = new ModelAndView("classes.html");
+        modelAndView.addObject("eazyClasses", eazyClasses);
         modelAndView.addObject("eazyClass", new EazyClass());
         return modelAndView;
     }
+
+    @PostMapping("/addNewClass")
+    public ModelAndView addNewClass(Model model, @ModelAttribute("eazyClass") EazyClass eazyClass) {
+        eazyClassRepository.save(eazyClass);
+        ModelAndView modelAndView = new ModelAndView("redirect:/admin/displayClasses");
+        return modelAndView;
+    }
+
+    @RequestMapping("/deleteClass")
+    public ModelAndView deleteClass(Model model, @RequestParam int id) {
+        Optional<EazyClass> eazyClass = eazyClassRepository.findById(id);
+        for(Person person : eazyClass.get().getPersons()) {
+            person.setEazyClass(null);
+            //removes foreign key between person and class.
+            personRepository.save(person);
+        }
+        eazyClassRepository.deleteById(id);
+        ModelAndView modelAndView = new ModelAndView("redirect:/admin/displayClasses");
+        return modelAndView;
+    }
+
+    @GetMapping("/displayStudents")
+    public ModelAndView displayStudents(Model model, @RequestParam int classId) {
+        ModelAndView modelAndView = new ModelAndView("students.html");
+        return modelAndView;
+    }
+
+
 }
