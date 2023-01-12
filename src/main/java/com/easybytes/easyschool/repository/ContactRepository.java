c57241bc2806d22ba83f24d91366e0ede0384d78
Phil Fernandez <philvfernandez@gmail.com>
2023-01-12 16:31:14 -0700

    Implemented demo of pagination and dynamic sorting - Part 1.

--- src/main/java/com/easybytes/easyschool/repository/ContactRepository.java
+++ src/main/java/com/easybytes/easyschool/repository/ContactRepository.java
@@ -1,16 +1,21 @@
 package com.easybytes.easyschool.repository;
 
 import com.easybytes.easyschool.model.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ContactRepository extends CrudRepository<Contact,Integer> {
+public interface ContactRepository extends PagingAndSortingRepository<Contact, Integer>, CrudRepository<Contact, Integer> {
 
     //Derived Query method in Spring Data JPA.
     //Abstract method that Spring JPA will have implementation at runtime.
     List<Contact> findByStatus(String status);
+
+    Page<Contact> findByStatus(String status, Pageable pageable);
 
 }
