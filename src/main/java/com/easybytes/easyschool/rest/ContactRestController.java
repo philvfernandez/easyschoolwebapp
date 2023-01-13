0d257cfe1a1b9be39aa3d855df1aa5a15ae90d2b
Phil Fernandez <philvfernandez@gmail.com>
2023-01-13 15:39:49 -0700

    Implemented REST service using spring MVC style and @ResponseBody - Part 2

--- src/main/java/com/easybytes/easyschool/rest/ContactRestController.java
+++ src/main/java/com/easybytes/easyschool/rest/ContactRestController.java
@@ -5,10 +5,7 @@ import com.easybytes.easyschool.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,16 @@ public class ContactRestController {
     @ResponseBody
     public List<Contact> getMessagesByStatus(@RequestParam(name = "status") String status) {
         return contactRepository.findByStatus(status);
+    }
+
+    @GetMapping("/getAllMsgsByStatus")
+    @ResponseBody
+    public List<Contact> getAllMsgsByStatus(@RequestBody Contact contact) {
+        if(contact != null && contact.getStatus() != null) {
+            return contactRepository.findByStatus(contact.getStatus());
+        } else {
+            return List.of();
+        }
+
     }
 }
