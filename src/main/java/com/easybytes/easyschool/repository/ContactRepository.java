e8d0475ad64640f9d203ba3d81afbc61a8df323d
Phil Fernandez <philvfernandez@gmail.com>
2023-01-13 10:00:11 -0700

    Added custom JPL and native query examples to the contact repository.

--- src/main/java/com/easybytes/easyschool/repository/ContactRepository.java
+++ src/main/java/com/easybytes/easyschool/repository/ContactRepository.java
@@ -3,6 +3,7 @@ package com.easybytes.easyschool.repository;
 import com.easybytes.easyschool.model.Contact;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -16,6 +17,10 @@ public interface ContactRepository extends PagingAndSortingRepository<Contact, I
     //Abstract method that Spring JPA will have implementation at runtime.
     List<Contact> findByStatus(String status);
 
+    //JPL Query example
+    //@Query("SELECT c FROM Contact c WHERE c.status = :status")
+    //Native sql query example
+    @Query(value = "SELECT * FROM contact_msg WHERE contact_msg.status = :status", nativeQuery = true)
     Page<Contact> findByStatus(String status, Pageable pageable);
 
 }
