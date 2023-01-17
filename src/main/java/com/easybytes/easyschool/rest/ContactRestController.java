f35851cc7dfa1b1cb6c893e657d021c3b587e956
Phil Fernandez <philvfernandez@gmail.com>
2023-01-17 11:02:26 -0700

    Implemented save operation using rest service and response entity.

--- src/main/java/com/easybytes/easyschool/rest/ContactRestController.java
+++ src/main/java/com/easybytes/easyschool/rest/ContactRestController.java
@@ -1,12 +1,16 @@
 package com.easybytes.easyschool.rest;
 
 import com.easybytes.easyschool.model.Contact;
+import com.easybytes.easyschool.model.Response;
 import com.easybytes.easyschool.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -31,4 +35,19 @@ public class ContactRestController {
         }
 
     }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom,
+                                            @Valid @RequestBody Contact contact) {
+        log.info(String.format("Header invocationFrom = %s", invocationFrom));
+        contactRepository.save(contact);
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message saved successfully");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("isMsgSaved", "true")
+                .body(response);
+    }
+
 }
