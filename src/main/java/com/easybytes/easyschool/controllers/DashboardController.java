acb04f2229a8013eae1eff7fab3a1898b03f72d2
Phil Fernandez <philvfernandez@gmail.com>
2023-01-18 14:43:35 -0700

    Added example of properties configurations to application properties files.

--- src/main/java/com/easybytes/easyschool/controllers/DashboardController.java
+++ src/main/java/com/easybytes/easyschool/controllers/DashboardController.java
@@ -4,6 +4,7 @@ import com.easybytes.easyschool.model.Person;
 import com.easybytes.easyschool.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,12 @@ public class DashboardController {
 
     @Autowired
     PersonRepository personRepository;
+
+    @Value("${easyschool.pageSize}")
+    private int defaultPageSize;
+
+    @Value("${easyschool.contact.successMsg}")
+    private String message;
 
     @RequestMapping("/dashboard")
     public String displayDashboard(Model model, Authentication authentication, HttpSession httpSession) {
@@ -44,5 +51,8 @@ public class DashboardController {
         log.info("Info message from the Dashboard page.");
         log.debug("Debug message from the Dashboard page.");
         log.trace("Trace message from the Dashboard page.");
+
+        log.error("defaultPageSize value with @Value annotation is: " + defaultPageSize);
+        log.error("successMsg value with @Value annotation is: " + message);
     }
 }
