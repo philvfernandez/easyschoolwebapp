1c28aeb77b0392d701cf2fff1b6050ea6512dc2c
Phil Fernandez <philvfernandez@gmail.com>
2023-01-18 11:50:44 -0700

    Corrected spelling of courses endpoint.

--- src/main/java/com/easybytes/easyschool/repository/CoursesRepository.java
+++ src/main/java/com/easybytes/easyschool/repository/CoursesRepository.java
@@ -2,11 +2,14 @@ package com.easybytes.easyschool.repository;
 
 import com.easybytes.easyschool.model.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(path = "courses")
+//@RepositoryRestResource(exported = false) //hides or disables courses endpoint.
 public interface CoursesRepository extends JpaRepository<Courses, Integer> {
 
     /*
